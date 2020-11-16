5e14f70b00b7f64485e056e76d4ba70d0fcd9fa2
moy <fabmoy1866@gmail.com>
2020-11-16 00:33:12 -0600

    Agregado archivo base examen.php

--- public_html/certifLogica.php
+++ public_html/certifLogica.php
@@ -17,11 +17,13 @@
     }else{
         if (!fueRealizado($_SESSION["user"],$archivoDatos)){
             //GUARDAR EN REALIZADOS
-            if (!fwrite($archivoDatos,$_SESSION["user"]."\n")){
+            //                                                 **********        DESCOMENTAR          **************
+            //                                                     ESTE IF AL TERMINAR DE DESARROLLAR EL EXAMEN
+            /*if (!fwrite($archivoDatos,$_SESSION["user"]."\n")){
                 echo "ERROR AL ESCRIBIR ARCHIVO TEST REALIZADOS";
-            }
+            }*/
             //PUEDE REALIZAR EL EXAMEN
-            echo "PUEDES REALIZAR TU EXAMEN ".$_SESSION["user"];
+            header("Location: examen.php");
         }else{
             // YA FUE REALIZADO
             header("Location: certificaciones.php");
